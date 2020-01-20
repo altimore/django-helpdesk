66ed51344b823cf03595c3f856a9ddea2d0e183c
Altimore <altimore9991@gmail.com>
2020-01-20 11:11:16 +0400

    Update forms.py

--- helpdesk/forms.py
+++ helpdesk/forms.py
@@ -9,7 +9,7 @@ forms.py - Definitions of newforms-based forms for creating and maintaining
 
 
 from django.core.exceptions import ObjectDoesNotExist
-from django.utils.six import StringIO
+from six import StringIO
 from django import forms
 from django.forms import widgets
 from django.conf import settings
